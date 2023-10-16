fdba4b45e79d8ede8591dedff03ff9370df32e13
jritama <jritamar@istecn.com.br>
2023-10-16 15:27:36 -0300

    ok

---
+++ Estudos/src/OperadoresJava.java
@@ -0,0 +1,10 @@
+public class OperadoresJava {
+    public static void main(String [] args){
+        double var01 = 18;
+        double var02 = 5;
+
+        double modulo = var01 / var02;
+
+        System.out.print(modulo);
+    }
+}
