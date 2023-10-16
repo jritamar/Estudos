86b894cff633f56997b7a2015e159849f2cc6a8b
Itamar Junior <jritamar@istecn.com.br>
2023-10-16 11:44:23 -0300

    Ok

--- Estudos/src/TiposVariaveis.java
+++ Estudos/src/TiposVariaveis.java
@@ -1,10 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        double salarioMinimo = 2500;
-        double gratificacaoSalario = 0.15;
 
-        double salarioFinal = salarioMinimo / (1 - gratificacaoSalario);
-        
-        System.out.println(salarioFinal);
+        String nomeFuncionario = "Itamar Junior ";
+        double salarioMinimo = 2500;
+        double gratificacaoSalario01 = 0.15;
+        double gratificacaoSalario02 = 0.20;
+        double metaVenda = 100;
+
+        if(metaVenda <= 100){
+            double salarioFinal = salarioMinimo / (1 - gratificacaoSalario01);
+            System.out.println("O salário de " + nomeFuncionario + "é "  + salarioFinal);
+        }else{
+            double salarioFinal = salarioMinimo / (1 - gratificacaoSalario02);
+            System.out.println("O salário de " + nomeFuncionario + "é "  + salarioFinal);
+        }                
     }
 }
